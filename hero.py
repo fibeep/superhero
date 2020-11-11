75c66c3a64c72ca56827639070dbcd3ee223b4cd
Salo <salomonco@gmail.com>
2020-11-11 12:54:00 -0400

    take damage done

--- hero.py
+++ hero.py
@@ -1,4 +1,6 @@
 from random import choice
+from ability import Ability
+from armor import Armor
 
 class Hero:
     # We want our hero to have a default "starting_health",
@@ -12,6 +14,8 @@ class Hero:
         # when a hero is created, their current health is
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
+        self.abilities = list()
+        self.armors = list()
     
 
     def fight(self, opponent):
@@ -19,13 +23,76 @@ class Hero:
         winner = choice(fighters)
         print(f'{winner} is the winner!')
 
+    def add_ability(self, ability):
+        self.abilities.append(ability)
 
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def defend(self, damage_amt):
+        total_block = 0
+        if self.armors != []:
+            for armor in self.armors:
+                total_block += armor.block()
+        return total_block - damage_amt
+
+    def take_damage(self, damage):
+        injury = damage - self.defend(damage)
+        self.current_health -= injury
+
+
+# ---------------------------- Testing ---------------------------
+# if __name__ == "__main__":
+#     # If you run this file from the terminal
+#     # this block is executed.
+#     hero1 = Hero("Wonder Woman")
+#     hero2 = Hero("Dumbledore")
+#     hero1.fight(hero2)
+
+# if __name__ == "__main__":
+#     # If you run this file from the terminal
+#     # this block is executed.
+#     my_hero = Hero("Grace Hopper", 200)
+#     print(my_hero.name)
+#     print(my_hero.current_health)
+
+# if __name__ == "__main__":
+#     # If you run this file from the terminal
+#     # this block is executed.
+#     ability = Ability("Great Debugging", 50)
+#     hero = Hero("Grace Hopper", 200)
+#     hero.add_ability(ability)
+#     hero.add_ability(ability)
+#     print(hero.abilities)
+
+# if __name__ == "__main__":
+#     # If you run this file from the terminal
+#     # this block of code is executed.
+#     ability = Ability("Great Debugging", 50)
+#     another_ability = Ability("Smarty Pants", 90)
+#     hero = Hero("Grace Hopper", 200)
+#     hero.add_ability(ability)
+#     hero.add_ability(another_ability)
+#     print(hero.attack())
 
 
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
+    # this block of code is executed.
 
-    hero1.fight(hero2)
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    hero.take_damage(50)
+    hero.take_damage(50)
+    hero.take_damage(50)
+
+
+    print(hero.current_health)
